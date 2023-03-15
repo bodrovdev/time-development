41997567a2f417a048f37e039c331a11b77850dd
Igor Bodrov <bodrovigor02@gmail.com>
2023-03-15 23:37:32 +0300

    feat: 🎸 Мобильное меню: адаптив, исправления

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,39 @@ import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 let burger = document.getElementById('burger');
 let mobile_menu = document.getElementById('mobile_menu');
 let nav_list = document.querySelector('.main-nav__nav-list');
+let search_bar = document.getElementById('search_bar');
+let page_cover = document.querySelector('.cover-default');
 
 // - Открытие по по клику на бургер
 burger.addEventListener('click', () => {
   burger.classList.toggle('main-nav__burger--active');
   mobile_menu.classList.toggle('main-nav__nav-menu--mobile--active');
-  document.body.classList.toggle('body-hidden');
+  page_cover.classList.toggle('cover-default--active');
 
   if (burger.classList.contains('main-nav__burger--active')) {
     disableBodyScroll(mobile_menu);
   }
   else {
+    enableBodyScroll(mobile_menu);
+  }
+})
+
+// - Закрытие по клику на неактивную часть меню
+window.addEventListener('click', (e) => {
+  if (!mobile_menu.classList.contains('main-nav__nav-menu--mobile--active')) {
+    return;
+  }
+  else if (
+    e.target !== mobile_menu &&
+    e.target.parentElement !== mobile_menu &&
+    e.target !== burger &&
+    e.target.parentElement !== burger &&
+    e.target !== search_bar &&
+    e.target.parentElement !== search_bar) {
+
+    mobile_menu.classList.remove('main-nav__nav-menu--mobile--active');
+    burger.classList.remove('main-nav__burger--active');
+    page_cover.classList.remove('cover-default--active');
     enableBodyScroll(mobile_menu);
   }
 })
